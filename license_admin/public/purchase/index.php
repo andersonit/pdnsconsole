08d9f1cc6c213f2be59dc196edf7fa2e9b62b910
Colin Anderson <andersonit@gmail.com>
2025-08-13 13:51:34 -0500

    Moving public site.

--- license_admin/public/purchase/index.php
+++
@@ -1,2 +0,0 @@
-Need to add purchase page, collecing name, email, company and create password for client account, then use Stripe API.
-Create account and redirect them to the login page to generate their key.
